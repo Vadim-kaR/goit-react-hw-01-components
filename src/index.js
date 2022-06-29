1ff0033737e6c7f94dc72f45e7e376e81d87361a
unknown <90862670+Vadim-kaR@users.noreply.github.com>
2022-06-29 13:53:27 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'App';
 import './index.css';
-import {ThemeProvider} from 'styled-components';
-import {theme} from './style_config/theme'
+import { ThemeProvider } from 'styled-components';
+import { theme } from './stylesConfig/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-        <App />
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
